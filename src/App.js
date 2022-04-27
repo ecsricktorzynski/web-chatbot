1aa93edfb38b15b9feadb889235f31d821d3cfb7
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-04-27 15:33:06 +0200

    init config (not working)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import Widget from "rasa-webchat";
 
 function App() {
   return (
     <div className="app-chat">
       chatbot
+      <Widget
+        initPayload={"/utter_greet"}
+        socketUrl={"http://localhost:5055/webhook"}
+        socketPath={"/socket.io/"}
+        customData={{ language: "en" }} // arbitrary custom data. Stay minimal as this will be added to the socket
+        title={"Title"}
+      />
     </div>
   );
 }
