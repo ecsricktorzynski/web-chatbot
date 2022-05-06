4e4411b0d1fd6ea0aa29db3676cbc1e35c434a81
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-05-06 09:33:47 +0200

    fix: widget works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
-import Widget from "rasa-webchat";
+import Widget from 'rasa-webchat'
 
 function App() {
   return (
     <div className="app-chat">
       chatbot
+
       <Widget
-        initPayload={"/utter_greet"}
-        socketUrl={"http://localhost:5055/webhook"}
+        initPayload={"/get_started"}
+        socketUrl={"http://localhost:5005"}
         socketPath={"/socket.io/"}
-        customData={{ language: "en" }} // arbitrary custom data. Stay minimal as this will be added to the socket
-        title={"Title"}
+        customData={{language: "en"}}
+        title={"Restaurant"}
+        showFullScreenButton={true}
+        displayUnreadCount={true}
+        showMessageDate={true}
+        storage="session"
       />
     </div>
   );
