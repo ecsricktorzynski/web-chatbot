63a66589a4e3f031f72bcde1057c7d322b98c1ec
Pawel Sokolowski <pawelsokolowski22222@gmail.com>
2022-05-06 15:42:54 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./App.css";
-import WidgetChat from './components/widget/WidgetChat'
+import WidgetChat from "./components/widget/WidgetChat";
 
 function App() {
   return (
-    <div className="app-chat">
-      chatbot
+    <div>
+      <h1>Try it out</h1>
+      <ul>
+        <li>book table - activate booking table form</li>
+        <li>rent - first node of response form about renting hall</li>
+        <li>cancel - first node of response form about canceling hall</li>
+        <li>catering - first node of response form about daily catering</li>
+      </ul>
+
       <WidgetChat />
     </div>
   );
